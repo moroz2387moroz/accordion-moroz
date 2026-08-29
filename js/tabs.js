a9b37c1be6a3d1a31023d2e9fba4a2ef09346761
Marina Moroz <moroz2387moroz@yandex.ru>
2026-08-29 15:52:23 +1000

    add: tabs

--- js/tabs.js
+++ js/tabs.js
@@ -1,1 +1,21 @@
-console.log("天官赐福");
+const tabsHandlerElems = document.querySelectorAll("[data-tabs-handler]");
+const tabsContentElems = document.querySelectorAll("[data-tabs-field]");
+const tabsTitleElems = document.querySelectorAll(".design__title");
+
+tabsHandlerElems.forEach((button) => {
+  button.addEventListener("click", () => {
+    tabsHandlerElems.forEach((item) => {
+      item.classList.toggle("design-list__item_active", item === button);
+    });
+
+    tabsContentElems.forEach((content) => {
+      const isActive = content.dataset.tabsField === button.dataset.tabsHandler;
+      content.classList.toggle("hidden", !isActive);
+    });
+
+    tabsTitleElems.forEach((title) => {
+      const isActive = title.dataset.tabsField === button.dataset.tabsHandler;
+      title.classList.toggle("hidden", !isActive);
+    });
+  });
+});
